a695a49e12b10edd786b71ab9f572ca3ec3c0144
Richard Abear <owchzzz@gmail.com>
2020-07-06 19:22:30 +0800

    Added initial notes test and graph endpoints

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
+    protected $fillable = ['note', 'contact_id'];
+    
     public function contact()
     {
         return $this->belongsTo(Contact::class);
---
+++ tests/Feature/NotesTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Note;
+use App\Models\Contact;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\AuthenticatedTestCase;
+
+class NotesTest extends AuthenticatedTestCase
+{
+    protected $contact;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->contact = new Contact([
+            'first_name' => 'Richard',
+            'last_name' => 'Abear',
+            'organization_id' => $this->user->permanentOrganization()->id
+        ]);
+
+        $this->contact->save();
+    }
+
+    public function testCanCreateNote()
+    {
+        $note = new Note([
+            'contact_id' => $this->contact->id,
+            'note' => 'Sample Note'
+        ]);
+
+        $note->save();
+        $this->assertEquals('Sample Note', $note->note);
+    }
+
+    public function testCanQueryCreateNote()
+    {
+        $noteData = [
+            'note' => 'Sample Note',
+            'contact_id' => $this->contact->id
+        ];
+
+        $this->graphQL('
+            mutation createNote($input: CreateNoteInput!) {
+                createNote(input: $input) {
+                    id
+                    note
+                    contact_id
+                }
+            }
+        ', ['input' => $noteData])->assertJson([
+            'data' => [
+                'createNote' => $noteData
+            ]
+        ]);
+    }
+}
